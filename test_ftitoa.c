a23cd6fa3b8be4f563958e0aa0813793f05c9008
AudeizReading <aaericia@hotmail.fr>
2021-04-17 18:17:01 +0200

    improve itoa test

--- test_ftitoa.c
+++ test_ftitoa.c
@@ -6,7 +6,7 @@
 /*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 13:44:48 by alellouc          #+#    #+#             */
-/*   Updated: 2021/04/17 17:21:38 by alellouc         ###   ########.fr       */
+/*   Updated: 2021/04/17 18:16:20 by alellouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,28 @@
 
 int		test_ftitoa(int n, char *expected)
 {
-	char	*itoa = ft_itoa(n);
+/*	char	*itoa = ft_itoa(n);
 
 	if (!strcmp(expected, itoa))
-		return (1);
+		return (1);*/
 	/* Need to detect heapleaks */
-	return (0);
+	int result = 0;
+	char	*ret = ft_itoa(n);
+
+	if (ret && expected)
+	{
+		if (!strcmp(ret, expected))
+		{
+			result = 1;
+		}
+	}
+	else if (ret == expected)
+	{
+					result = 1;
+	}
+	/* Si ca segfault, il est possible que ret ne soit pas malloc -> c'est une
+	** erreur */
+	free(ret);
+	return (result);
+/*	return (0);*/
 }
